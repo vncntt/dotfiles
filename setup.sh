f8cf6de34a042ebf7ba5e872f1715072700ef195
vncntt <provincent8412@gmail.com>
2025-10-07 11:01:38 -0400

    add setup.sh and .vimrc

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# setup.sh
+
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+
+mkdir -p ~/.config/fish
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+echo "dotfiles symlinked"
